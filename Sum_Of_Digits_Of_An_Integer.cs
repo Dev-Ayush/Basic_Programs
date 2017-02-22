0d138e36c182d4662ea2a374df98bf89759eaa28
Ayush Kumar Singh <ayushaks14@gmail.com>
2017-02-22 13:39:25 +0530

    Add files via upload

---
+++ Sum_Of_Digits_Of_An_Integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum=0, r = 0;
+            while(number!=0)
+            {
+                r = number % 10;
+                number = number / 10;
+                sum = sum + r;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
